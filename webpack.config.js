eb928a75b4fa322b30604328acb59dc91768a5d4
OEstebanVera2367 <88457031+vera2367@users.noreply.github.com>
2023-05-09 11:57:34 -0500

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -8,5 +10,43 @@ module.exports = {
     },
     resolve: {
         extensions: ['.js']
-    }
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude:/node_modules/,
+            use: {
+                loader: 'babel-loader'
+                 }
+          },
+          {
+            test: /\.css|.styl$/i,
+            use: [MiniCssExtractPlugin.loader,
+                'css-loader',
+                'stylus-loader'
+                ],
+          },
+          {
+            test:/\.png/,
+            type:'asset/resource'
+          }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname,"src", "assets/images"),
+                    to: "assets/images"
+                }
+            ]
+        })
+    ]
 }
